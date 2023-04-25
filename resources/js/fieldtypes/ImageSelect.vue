6303783051a5189d2c7b900220789f561a2085e3
patrickjunod <patrick@junod.ch>
2023-04-25 17:24:43 +0200

    Add: Multiple choice selection

--- resources/js/fieldtypes/ImageSelect.vue
+++ resources/js/fieldtypes/ImageSelect.vue
@@ -1,18 +1,18 @@
 <template>
-    <fieldset class="flex items-center space-x-2">
-        <label :class="{ 'ring-offset-1 border-blue-500 scale-[102%]' : value === option.image_value, 'scale-[100%] border-opacity-10' : value !== option.image_value }" class="relative rounded border-2 transition-all flex flex-col cursor-pointer items-center justify-center px-1 py-0.5 focus:outline-none ring-blue-500"
+    <fieldset class="flex items-center space-x-2 select-none">
+        <label :class="exists(option.image_value) ? 'ring-offset-1 border-blue-500 scale-[102%]' : 'scale-[100%] border-opacity-10'" class="relative rounded border-2 transition-all flex flex-col cursor-pointer items-center justify-center px-1 py-0.5 focus:outline-none ring-blue-500"
                v-for="(option, index) in options" :key="$index">
-            <input type="radio"
+            <input type="checkbox"
                    ref="images"
-                   :name="name"
-                   @input="update($event.target.value)"
                    :value="option.image_value"
+                   :true-value="option.image_value"
+                   :false-value="null"
+                   v-model="values"
                    :disabled="isReadOnly"
-                   :checked="value === option.image_value"
                    class="sr-only"
                    :aria-labelledby="name + '-' + index + '-label'"
             />
-            <span v-if="value === option.image_value" class="absolute backdrop-blur flex items-center justify-center h-12 w-12 border border-gray-200 bg-white/60 text-gray-500 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <span v-if="exists(option.image_value)" class="absolute backdrop-blur flex items-center justify-center h-8 w-8 border-2 border-blue-500 bg-blue-500/70 text-blue-50 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                 </svg>
@@ -30,14 +30,26 @@ export default {
 
     mixins: [Fieldtype],
 
+    data() {
+        return {
+            values: ((this.config.multiple && !Array.isArray(this.value)) ? [this.value] : this.value) || (this.config.multiple ? [] : ''),
+        }
+    },
+
     computed: {
+
         options() {
             return this.meta.options;
         },
 
         replicatorPreview() {
-            var option = _.findWhere(this.config.options, {value: this.value});
-            return (option) ? option.label : this.value;
+            return Array.isArray(this.values)
+                    ?  this.values.map(value => {
+                            const option = _.findWhere(this.options, { value });
+                            return option ? option.label : value;
+                        }).join(', ')
+                    : this.values;
+
         },
     },
 
@@ -47,7 +59,31 @@ export default {
             this.$refs.images[0].focus();
         },
 
-    }
+        exists(value) {
+            if(this.config.multiple && Array.isArray(this.values)) {
+                return this.values.includes(value)
+            }
+
+            return this.values === value;
+        }
+
+    },
+
+    watch: {
+
+        values(values, oldValues) {
+
+            this.update(values);
+
+        },
+
+        value(value, oldValue) {
+
+            this.values = value;
+
+        }
+
+    },
 
 
 };
